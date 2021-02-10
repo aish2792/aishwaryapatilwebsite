9a693982078d15e6eaa9a9522a27f037f95c37a8
Aishwarya Patil <patilaish2792@gmail.com>
2021-02-09 23:01:02 -0500

    bootstrap styling

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import MovieReviews from './components/MovieReviews';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faTwitter, faGithub, faInstagramSquare , faGoogle} from "@fortawesome/free-brands-svg-icons";
 
 
 
@@ -18,22 +20,75 @@ import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-do
 function App() {
   return (
     <Router>
-      <div className="App" style={{display: 'flex', flexDirection: 'column', flex: 1}}>
+      {/* <div className="App" style={{display: 'flex', flexDirection: 'column', flex: 1}}> */}
+      <div className='container'>
+      
 
         {/* <Header /> */}
         <Route path='/' exact component={AboutMe}/>
         {/* <Route path='/' exact component={Experience}/> */}
         <Route path='/Projects' component={Projects}/>
-        <Route path='/Unxpired' component={Unxpired}/>
+        {/* <Route path='/Unxpired' component={Unxpired}/>
         <Route path='/LoanPrediction' component={LoanPrediction}/>
         <Route path='/Kaleidoscope' component={Kaleidoscope}/>
-        <Route path='/MovieReviews' component={MovieReviews}/>
+        <Route path='/MovieReviews' component={MovieReviews}/> */}
         {/* <Route path='/aboutme'   component={AboutMe}/> */}
-        <Footer />
+        
 
-    </div>
+    
+        </div>
+        <div class="clear"></div>
+        <footer className="footer fixed-bottom">
+        <div className="container">
+          <div className="row justify-content-center rowStyle">
 
+            <div className="col-xs-12"> 
+              <a
+                  className="faLinkedin app-link social-media-list "
+                  href="https://www.linkedin.com/in/aishwarya-patil2792"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" className='iconStyle'/>
+              </a>
+
+       
+         
+            <a
+              className="faGithub app-link social-media-list iconPadding"
+              href="https://github.com/aish2792/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              <FontAwesomeIcon icon={faGithub} size="2x" className='iconStyle'/>
+            </a>
+
+          
+
+            <a
+              className="fatwitter app-link social-media-list"
+              href = "mailto:aishp2792@gmail.com?subject = Feedback&body = Message"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              <FontAwesomeIcon icon={faGoogle} size="2x" className='iconStyle'/>
+            </a>
+
+
+            </div>
+            
+          </div>
+          <div className='row justify-content-center'>
+          <p className="footerNote">© 2020-present designed by Aishwarya Patil</p>
+          </div>
+          
+          
+          
+        </div>
+      </footer>
     </Router>
+    
     
   );
 }
