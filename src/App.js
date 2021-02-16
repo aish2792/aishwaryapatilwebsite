c2c24080066b27ba3941ec8741d0b148a256b500
Aishwarya Patil <patilaish2792@gmail.com>
2021-02-16 16:48:46 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Introduction from './components/Introduction';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import Unxpired from './components/Unxpired';
-import LoanPrediction from './components/LoanPrediction';
-import Kaleidoscope from './components/Kaleidoscope';
-import MovieReviews from './components/MovieReviews';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faTwitter, faGithub, faInstagramSquare , faGoogle} from "@fortawesome/free-brands-svg-icons";
-
-
-
+import { faLinkedin, faGithub , faGoogle} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <Router>
-      {/* <div className="App" style={{display: 'flex', flexDirection: 'column', flex: 1}}> */}
       <div className='container'>
-      
-
-        {/* <Header /> */}
         <Route path='/' exact component={AboutMe}/>
-        {/* <Route path='/' exact component={Experience}/> */}
         <Route path='/Projects' component={Projects}/>
-        {/* <Route path='/Unxpired' component={Unxpired}/>
-        <Route path='/LoanPrediction' component={LoanPrediction}/>
-        <Route path='/Kaleidoscope' component={Kaleidoscope}/>
-        <Route path='/MovieReviews' component={MovieReviews}/> */}
-        {/* <Route path='/aboutme'   component={AboutMe}/> */}
         
-
-    
         </div>
         <div class="clear"></div>
         <footer className="footer fixed-bottom">
